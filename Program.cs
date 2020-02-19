fca618c7a127b419ab28584f0a7a44ce8fdd8b14
kyleb <kyleb@DESKTOP-LDJID95>
2020-02-18 23:25:31 -0500

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Deliverable___Interative_Statement___Kyle_Bigelow
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter an integer between 2 and 10");
+
+            int n = Convert.ToInt32(Console.ReadLine());
+
+            if (n % 2 == 1)
+            {
+                for (int i = 0; i < n - 1; i++)
+                    Console.WriteLine("You entered an odd number");
+            }
+            else
+            {
+                for(int i = 0; i < n + 1; i++)
+                    Console.WriteLine("You entered an even number");
+            }
+        }
+        
+       
+    }
+}
